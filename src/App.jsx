4752796b7bdd0b581042d300d949a45c1591d37c
Beatriz <beatrizcabdias@gmail.com>
2026-08-08 17:07:28 -0300

    feat: add Typography, Card, and DailyBudget components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Container } from './components/Container'
 import { Aside } from './components/Aside'
 import { SearchInput } from './components/SearchInput'
 import { Main } from './components/Main'
+import { Typography } from './components/Typography'
+import Card from './components/Card'
+import { DailyBudget } from './components/DailyBudget'
 
 function App() {
   return (
@@ -9,6 +12,29 @@ function App() {
       <Aside />
         <Main>
           <SearchInput />
+          <div>
+            <Typography variant="h1">
+              Olá, Vinny!
+            </Typography>
+            <Typography variant="body">
+              Veja como estão suas finanças hoje.
+            </Typography>
+            <section>
+              <Card>
+                <Card.Header>
+                  Orçamento diário disponível:
+                </Card.Header>
+                <Card.Body>
+                  <DailyBudget value={200}/>
+
+                </Card.Body>
+
+              </Card>
+            </section>
+          </div>
+
+
+
 
         </Main>
     </Container>
