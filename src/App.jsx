e786c012cd49fab751d3bda5e2a5bd21ac705f6d
Beatriz <beatrizcabdias@gmail.com>
2026-08-08 14:40:51 -0300

    feat: add Main and SearchInput components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { Container } from './components/Container'
 import { Aside } from './components/Aside'
+import { SearchInput } from './components/SearchInput'
+import { Main } from './components/Main'
 
 function App() {
   return (
     <Container>
-      <Aside/>
+      <Aside />
+        <Main>
+          <SearchInput />
+
+        </Main>
     </Container>
   )
 }
