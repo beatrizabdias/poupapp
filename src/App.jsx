eeeb6a1f1797d80bdb2d6e5031c377d31044ca8f
Beatriz <beatrizcabdias@gmail.com>
2026-09-11 19:10:03 -0300

    feat: implement financial transactions layout and component

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,63 @@ import { Typography } from './components/Typography'
 import Card from './components/Card'
 import { DailyBudget } from './components/DailyBudget'
 import { SavingStatus } from './components/SavingStatus'
+import { Transactions } from './components/Transactions'
+
+import styles from './app.module.css';
 
 function App() {
   return (
     <Container>
       <Aside />
         <Main>
-          <SearchInput />
-          <div>
-            <Typography variant="h1">
-              Olá, Vinny!
-            </Typography>
-            <Typography variant="body">
-              Veja como estão suas finanças hoje.
-            </Typography>
-            <section>
-              <Card>
-                <Card.Header>
-                  Orçamento diário disponível:
-                </Card.Header>
-                <Card.Body>
-                  <DailyBudget value={200}/>
-                </Card.Body>
-              </Card>
-              <Card>
-                <Card.Header>
-                  Progresso da meta financeira
-                </Card.Header>
-                <Card.Body>
-                  <SavingStatus percent={40} />
-                </Card.Body>
-              </Card>
+          <div className={styles.container}>
+            <SearchInput />
+            <div>
+              <Typography variant="h1">
+                Olá, Vinny!
+              </Typography>
+              <Typography variant="body">
+                Veja como estão suas finanças hoje.
+              </Typography>
+            </div>
+            <section className={styles.grid}>
+                <Card>
+                  <Card.Header>
+                    Orçamento diário disponível:
+                  </Card.Header>
+                  <Card.Body>
+                    <DailyBudget value={200}/>
+                  </Card.Body>
+                </Card>
+                <Card>
+                  <Card.Header>
+                    Progresso da meta financeira
+                  </Card.Header>
+                  <Card.Body>
+                    <SavingStatus percent={40} />
+                  </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Header>
+                      Movimentação Financeira
+                    </Card.Header>
+                    <Card.Body>
+                      <Transactions/>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Header>
+                      Minhas contas
+                    </Card.Header>
+                    <Card.Body>
+                      <p>R$ 200</p>
+                      <p>R$ 200</p>
+                      <p>R$ 200</p>
+                      <p>R$ 200</p>
+                      <p>R$ 200</p>
+                    </Card.Body>
+                </Card>
+                
             </section>
           </div>
         </Main>
